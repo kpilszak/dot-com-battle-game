63c3290c87b61b0fdd6ca3e7edb592e6f3c5139a
kpilszak <klaudia.pilszak@gmail.com>
2019-02-05 21:54:16 +0100

    Add instance variables in DotCom

--- src/DotCom.java
+++ src/DotCom.java
@@ -1,4 +1,12 @@
 public class DotCom {
+
+    int[] locationCells;
+    int numOfHits = 0;
+
+    public void setLocationCells(int[] locs) {
+        locationCells = locs;
+    }
+
     public String checkYourself(String stringGuess) {
         int guess = Integer.parseInt(stringGuess);
         String result = "miss";
