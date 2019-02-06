63d381b44ea2aeaae81dcd715525f1a45d26fd94
kpilszak <klaudia.pilszak@gmail.com>
2019-02-06 04:39:08 +0100

    Correct main()

--- src/DotComTestDrive.java
+++ src/DotComTestDrive.java
@@ -1,14 +1,20 @@
 public class DotComTestDrive {
     public static void main(String[] args) {
-        DotCom dot = new DotCom();
-        int[] locations = {2,3,4};
-        dot.setLocationCells(locations);
-        String userGuess = "2";
-        String result = dot.checkYourself(userGuess);
-        String testResult = "failed";
-        if (result.equals("hit")) {
-            testResult = "passed";
+        int numOfGuesses = 0;
+        GameHelper helper = new GameHelper();
+        DotCom theDotCom = new DotCom();
+        int randomNum = (int) (Math.random() * 5);
+        int[] locations = {randomNum, randomNum+1, randomNum+2};
+        theDotCom.setLocationCells(locations);
+        boolean isAlive = true;
+        while (isAlive == true) {
+            Sring guess = helper.getUserInput("enter a number");
+            String result = theDotCom.checkYourself(guess);
+            numOfGuesses++;
+            if (result.equals("kill")) {
+                isAlive = false;
+                System.out.println("You took " + numOfGuesses + " guesses");
+            }
         }
-        System.out.println(testResult);
     }
 }
