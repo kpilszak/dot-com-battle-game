e52c2d60252368435953a82a591637f338e5cc34
kpilszak <klaudia.pilszak@gmail.com>
2019-02-06 06:00:03 +0100

    Use ArrayList<String> instead regular array

--- src/DotCom.java
+++ src/DotCom.java
@@ -1,26 +1,25 @@
+import java.util.ArrayList;
+
 public class DotCom {
 
-    int[] locationCells;
+    private ArrayList<String> locationCells;
     int numOfHits = 0;
 
-    public void setLocationCells(int[] locs) {
-        locationCells = locs;
+    public void setLocationCells(ArrayList<String> loc) {
+        locationCells = loc;
     }
 
-    public String checkYourself(String stringGuess) {
-        int guess = Integer.parseInt(stringGuess);
+    public String checkYourself(String userInput) {
         String result = "miss";
-        for (int cell : locationCells) {
-            if (guess == cell) {
+        int index = locationCells.indexOf(userInput);
+        if (index >= 0) {
+            locationCells.remove(index);
+            if (locationCells.isEmpty()) {
+                result = "kill";
+            } else {
                 result = "hit";
-                numOfHits++;
-                break;
             }
         }
-        if (numOfHits == locationCells.length) {
-            result = "kill";
-        }
-        System.out.println(result);
         return result;
     }
 }
